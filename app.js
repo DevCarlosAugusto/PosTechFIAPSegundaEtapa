330eb12792f1d588c0733dc37fd185ce1742157d
Carlos Augusto <carlos.xell.3@gmail.com>
2025-09-19 10:56:40 -0300

    FEATURE: added new structure, added new packages and rewrited node scripts

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+import indexRouter from './src/routes/index.js';
+import usersRouter from './src/routes/users.js';
+
+// Obtenha o caminho do diretório atual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 let app = express();
 
@@ -24,18 +30,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
-module.exports = app;
+export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-let express = require('express');
+import express from 'express';
+
 let router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+export default router;
