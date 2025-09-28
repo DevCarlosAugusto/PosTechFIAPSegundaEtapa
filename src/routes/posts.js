1771819485b038f7e7d8e317a9acfa73f17ff4fc
Raquel Brombilla <raquelbrombilla11@gmail.com>
2025-09-28 11:15:50 -0300

    FEATURE: Adicionado rotas para gerenciamento de posts

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,14 @@
 import PostsController from '../controllers/PostsController.js';
-
+import { withDbInit } from '../middleware/withDbInit.js';
 import { Router } from 'express';
 
 const router = Router();
 
-router.get("/", PostsController.getAllPosts);
-router.get("/:id", PostsController.getPostById);
+router.get("/", withDbInit(PostsController.getAllPosts));
+router.get("/search", withDbInit(PostsController.searchPosts)); 
+router.get("/:id", withDbInit(PostsController.getPostById));
+router.post("/", withDbInit(PostsController.createPost));
+router.put("/:id", withDbInit(PostsController.updatePost)); 
+router.delete("/:id", withDbInit(PostsController.deletePost)); 
 
 export default router;
